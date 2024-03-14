294edc006d5cd19cec030658da87ae175bcf39fe
Ubuntu <ubuntu@ip-172-31-21-117.ec2.internal>
2024-03-14 21:25:27 +0000

    Node error fix attempt 1

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
-sudo apt update && sudo apt install nodejs npm
+sudo apt update && sudo apt update -y && sudo apt install nodejs npm
+
+#checking node version to fix error npm v10.5.0 is known not to run on Node.js v10.19.0.  This version of npm supports the following node versions: `^18.17.0 || >=20.5.0`. You can find the latest version at https://nodejs.org/.
+node -v 
+sudo nvm install 18
+sudo nvm use 18
+node -v
+npm -v
 
 sudo npm install -g pm2
 
